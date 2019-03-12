c11f8361a8b6c64ed80d38451cee8bf8f29f7b19
romain sanson <romainsanson.gm@gmail.com>
2019-03-12 14:02:59 +0100

    script tweak

--- script.js
+++ script.js
@@ -13,64 +13,42 @@ const search = () => {
   goWiki(word)
 }
 
-inputField.addEventListener('change', (event) => {
+inputField.addEventListener('input', (event) => {
   results.innerHTML = ''
   search();
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //////// INSERT DATA FUNCTION //////////
 
 const insertData = (data) => {
   for (var i = 0; i < data.length; i++) {
-    const result = `
-<div class="column is-one-quarter">
-  <div class="card">
-    <div class="card-image">
-      <figure class="image is-4by3">
-        <img src="${data[i].thumbnail ? data[i].thumbnail.source : 'https://bulma.io/images/placeholders/96x96.png'}" alt="Placeholder image">
-      </figure>
-    </div>
-
-    <div class="card-content">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-          </figure>
-        </div>
-
-        <div class="media-content">
-          <p class="title is-4">${data[i].title ? data[i].title : '...'}</p>
-          <p class="subtitle is-6">@johnsmith</p>
-        </div>
+    if (data[i].thumbnail) {
+      const result = `
+  <div class="column is-one-quarter">
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="${data[i].thumbnail.source}" alt="Placeholder image">
+        </figure>
       </div>
 
-      <div class="content">
-        ${data[i].terms ? data[i].terms.description[0] : '...'}
-        <a href="${data[i].title ? 'https://fr.wikipedia.org/wiki/' + data[i].title.replace(/\s/, '_') : '#'}">Go Wiki</a>
-        <br>
-        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+      <div class="card-content">
+        <div class="media">
+
+          <div class="media-content">
+            <p class="title is-4">${data[i].title ? data[i].title : '...'}</p>
+          </div>
+        </div>
+
+        <div class="content">
+          ${data[i].terms ? data[i].terms.description[0] : '...'}
+          <a href="${data[i].title ? 'https://fr.wikipedia.org/wiki/' + data[i].title.replace(/\s/, '_') : '#'}">Go Wiki</a>
+          <br>
+        </div>
       </div>
     </div>
-  </div>
-</div>`;
-    results.insertAdjacentHTML('beforeend', result)
+  </div>`;
+      results.insertAdjacentHTML('beforeend', result)
+    }
   }
 };
